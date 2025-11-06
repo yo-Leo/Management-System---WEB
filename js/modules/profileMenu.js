fdbc1acb82aacec2502c5e257b460843553770c1
Leonardo Ruas <leonardorodriguesruas7@gmail.com>
2025-11-06 01:52:22 -0300

    adicionando tela de cadastro de usuario

--- js/modules/profileMenu.js
+++ js/modules/profileMenu.js
@@ -4,22 +4,19 @@ export function initProfileMenu() {
   const addUserBtn = document.getElementById("addUserBtn");
   const logoutBtn = document.getElementById("logoutBtn");
 
-  if (!profileImage || !menu) return; // Evita erros se o header não existir
+  if (!profileImage || !menu) return;
 
-  // Alterna o menu ao clicar na imagem
   profileImage.addEventListener("click", (e) => {
     e.preventDefault();
     menu.classList.toggle("hidden");
   });
 
-  // Fecha o menu ao clicar fora
   document.addEventListener("click", (e) => {
     if (!menu.contains(e.target) && e.target !== profileImage) {
       menu.classList.add("hidden");
     }
   });
 
-  // Eventos dos botões
   if (addUserBtn) {
     addUserBtn.addEventListener("click", () => {
       alert("Função: Adicionar usuário");
